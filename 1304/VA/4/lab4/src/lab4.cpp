4b3b9ac997690d39a4f38266206c39bf6bb9d939
sasha4052 <sasha4052@yandex.ru>
2016-12-06 02:37:02 +0300

    lab4 fix

--- 1304/VA/4/lab4/src/lab4.cpp
+++ 1304/VA/4/lab4/src/lab4.cpp
@@ -17,7 +17,7 @@ float nxpose=0;
 float nypose=0;
 float norient=0;
 float targetx=10;//-2.50712;
-float targety=10;//-1.42;
+float targety=5;//-1.42;
 bool goal=false;
 bool goToSpin=false;
 float distTogoal=100;
@@ -32,7 +32,7 @@ struct direction
 struct command
 {
     static const int goFoward=1;
-	static const int goRight=2;
+    static const int goRight=2;
     static const int goLeft=3;
     static const int goBack=4;
 
@@ -150,134 +150,158 @@ bool crashListener(float angleToTr)
 
     if(clount>=100) {ROS_ERROR_STREAM("sasasa"<<clount); clount=0; return true;};
 }
+
+struct mode
+{
+    static const int goToTarget=1;
+    static const int topBack=2;
+    static const int rightLeft=3;
+    static const int startDrive=4;
+
+
+};
+int currMode=1;
+bool turnTopBackProc=false;
+int currLeftRgit=command::goLeft;
+int currTopBack=command::goFoward;
+bool turnTop()
+{
+    //ROS_ERROR_STREAM("t(: ");
+    if((fabs(anglePose-(anglePose/fabs(anglePose))*pi/2)>0.05)&&(currMode==mode::topBack))
+    {
+
+         turn(direction::LEFT); //по часовой
+         return false;
+    } else return true;
+
+}
+bool turnBack()
+{
+   // ROS_ERROR_STREAM("b(: ");
+    if((fabs(anglePose-pi/2)>0.05)&&(currMode==mode::topBack))
+    {
+
+         turn(direction::LEFT); //по часовой
+         return false;
+    } else return true;
+
+}
+bool turnleftRightProc=false;
+bool turnLeft()
+{
+   // ROS_ERROR_STREAM("l(: ");
+    if((fabs(anglePose-(anglePose/fabs(anglePose))*pi) > 0.05)&&(currMode==mode::rightLeft))
+    {
+
+         turn(direction::RIGHT); //по часовой
+         false;
+
+    }else {currMode=mode::startDrive; return true;};
+}
+bool turnRight()
+{
+    //ROS_ERROR_STREAM("r(: ");
+    if((fabs(anglePose) > 0.05)&&(currMode==mode::rightLeft))
+    {
+
+         turn(direction::RIGHT); //по часовой
+         false;
+
+    }else {currMode=mode::startDrive; return true;};
+}
+
 bool robotAction()
 {
-    float angleToTarget=(getAngleToTarget()+pi);
-            while (angleToTarget>pi) angleToTarget-=2*pi;
-            while (angleToTarget<-pi) angleToTarget+=2*pi;
-   ROS_ERROR_STREAM("n(: "<<angleToTarget);
-  //ROS_ERROR_STREAM("n(: "<<nxpose<<","<<nypose);
-   ROS_ERROR_STREAM("t(: "<<"|"<<xpose<<","<<ypose );
-   ROS_ERROR_STREAM("curcom(: |"<<currentCommand );
 
-  // ROS_ERROR_STREAM("k(: "<<"|"<<targetx<<","<<targety );
-   /* if(crashListener(angleToTarget))
-    {
-        if(cldir==1) { turn(2); cldir=2;}
-        else { turn(1); cldir=1;};
-        step=0;
-        return false;
-    }*/
-    if(currentCommand==command::goFoward)
-    {
-      if (distTogoal < 0.1) {
-            return true;
-        }
-        if (fabs(angleToTarget) > 0.05) {
-        if (angleToTarget < 0.0) {
-            turn(direction::RIGHT);
-        } else {
-            turn(direction::LEFT);
-        }
-        ROS_INFO("Angle %f", angleToTarget);
-        } else {
-        if (isForward) {
-            drive(1.0f);
-        } else if (isLeft) {
-            currentCommand=command::goLeft;
-            step=0;
-        } else if (isRight) {
-            currentCommand=command::goRight;
-            step=0;
-        } else {
-            currentCommand=command::goBack;
-            step=0;
-        }
-        }
-        return false;
-    }
-    if(currentCommand==command::goLeft)
-    {
-        if (fabs(-pi/2 - angleToTarget) > 0.05) {
-            if ((-pi/2 - angleToTarget) < 0.0) {
-                turn(direction::LEFT);
-            } else {
-                turn(direction::RIGHT);
-            }
-          //  ROS_INFO("Angle %f", angleToTarget);
-            } else {
-                if (step < maxSteps) {
-                    drive(3.0f);
-                    step++;
-                } else {
-                    if (isRight) {
-                       currentCommand=command::goFoward;
-                       step=0;
-                    } else if (isForward) {
-                        drive(1.0f);
-                    } else if (isLeft) {
-                        currentCommand=command::goBack;
-                        step=0;
-                    } else {
-                        currentCommand=command::goRight;
-                        step=0;
-                    }
-                 }
-            }
-        return false;
-    }
-    if(currentCommand==command::goRight)
-    {
-        if (fabs(pi/2 - angleToTarget) > 0.05) {
-        if ((pi/2 - angleToTarget) < 0.0) {
-            turn(direction::LEFT);
-        } else {
-            turn(direction::RIGHT);
-        }
-       // ROS_INFO("Angle %f", angleToTarget);
-        } else {
-            if (step < maxSteps) {
-                drive(3.0f);
-                step++;
-            } else {
-                if (isLeft) {
-                    currentCommand=command::goFoward;
-                    step=0;
-                } else if (isForward) {
-                    drive(1.0f);
-                } else if (isRight) {
-                    currentCommand=command::goBack;
-                    step=0;
-                } else {
-                    currentCommand=command::goLeft;
-                    step=0;
-                }
-            }
-        }
-        return false;
-    }
-    if(currentCommand==command::goBack)
-    {
-        if (fabs(pi - fabs(angleToTarget)) > 0.05) {
-        if ((pi - fabs(angleToTarget)) < 0.0) {
-            turn(direction::LEFT);
-        } else {
-            turn(direction::RIGHT);
-        }
-      //  ROS_INFO("Angle %f", angleToTarget);
-        } else {
-            if (isLeft) {
-               currentCommand=command::goRight;
-               step=0;
-            } else if (isRight) {
-            currentCommand=command::goLeft;
-            step=0;
-            } else {
-            drive(1.0f);
-            }
-        }
-        return false;
-    }
+    float angleToTarget=(getAngleToTarget()+pi);
+    while (angleToTarget>pi) angleToTarget-=2*pi;
+    while (angleToTarget<-pi) angleToTarget+=2*pi;
+   //ROS_ERROR_STREAM("n(: "<<angleToTarget);
+  // ROS_ERROR_STREAM("apose(: "<<anglePose<<" M:"<<currMode);
+  //ROS_ERROR_STREAM("n(: "<<nxpose<<","<<nypose);
+  // ROS_ERROR_STREAM("t(: "<<"|"<<xpose<<","<<ypose );
+   //ROS_ERROR_STREAM("curcom(: |"<<currentCommand );
+  // ROS_ERROR_STREAM("dif(: "<<fabs(anglePose+pi/2)<<" M:"<<currMode);
+
+  if (distTogoal < 0.1) {
+      return true;
+  }
+  if(currMode==mode::goToTarget)
+  {
+
+      if(fabs(angleToTarget) > 0.05)
+      {
+
+           turn(direction::LEFT); //против часовой
+
+      }else {
+         if(isForward)
+         {
+             drive(1.0f);
+         }
+         else {turnTopBackProc=false; currMode=mode::topBack;};
+
+      }
+      return false;
+
+  }
+  if((currMode==mode::topBack)||(currMode==mode::rightLeft)||(currMode==mode::startDrive))
+  {
+      if(!turnTopBackProc)
+      {
+          if(currTopBack==command::goFoward)
+          {
+              turnTopBackProc=turnTop();
+          }else
+          {
+              turnTopBackProc=turnBack();
+          }
+
+      }else
+      {
+          if(isForward && !( currMode==mode::rightLeft))
+          {
+              drive(1.0f);
+              if(step==19) {currMode=mode::goToTarget; step=0; return false;}
+              step++;
+          } else
+          {
+                  if(currMode==mode::topBack && step!=0){
+                  if (currTopBack==command::goFoward) {
+                      currTopBack=command::goBack;
+                      if(currLeftRgit==command::goLeft) currLeftRgit=command::goRight;
+                      else currLeftRgit=command::goLeft;
+                  }
+                  else {
+                      currTopBack=command::goFoward;
+                      if(currLeftRgit==command::goLeft) currLeftRgit=command::goRight;
+                      else currLeftRgit=command::goLeft;
+                  }
+                  ROS_ERROR_STREAM("chageDirTopBack(: "<<currTopBack);
+              }else{
+                  if(turnleftRightProc && currMode==mode::startDrive)  {
+                      if(currLeftRgit==command::goLeft) currLeftRgit=command::goRight;
+                      else currLeftRgit=command::goLeft;
+                      ROS_ERROR_STREAM("chageRightlef(: "<<currLeftRgit);
+                  }
+
+              };
+              turnleftRightProc=false;
+              currMode=mode::rightLeft;
+              step=0;
+
+              if(currLeftRgit==command::goLeft)
+              {
+                 turnleftRightProc=turnLeft();
+              }else
+              {
+                 turnleftRightProc=turnRight();
+              }
+
+          }
+      }
+   return false;
+  }
 }
 
 void  searchingGoal(ros::NodeHandle node)
@@ -294,7 +318,7 @@ void  searchingGoal(ros::NodeHandle node)
 
 
 int main(int argc, char** argv) {
-  ros::init(argc, argv, "lab4");
+  ros::init(argc, argv, "teleop");
 
   ros::NodeHandle node;
   velocity_publisher = node.advertise<geometry_msgs::Twist>("/cmd_vel", 1000);
