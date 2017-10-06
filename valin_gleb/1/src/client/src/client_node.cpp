0683819ea600c6a163ec6a400cbfa2f8cd975e0b
Gleb Valin <the7winds@yandex.ru>
2017-10-06 15:14:03 +0300

    format

--- valin_gleb/1/src/client/src/client_node.cpp
+++ valin_gleb/1/src/client/src/client_node.cpp
@@ -8,16 +8,16 @@
 
 int getch()
 {
-  static struct termios oldt, newt;
-  tcgetattr( STDIN_FILENO, &oldt);           // save old settings
-  newt = oldt;
-  newt.c_lflag &= ~(ICANON);                 // disable buffering
-  tcsetattr( STDIN_FILENO, TCSANOW, &newt);  // apply new settings
+    static struct termios oldt, newt;
+    tcgetattr(STDIN_FILENO, &oldt); // save old settings
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON);               // disable buffering
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt); // apply new settings
 
-  int c = getchar();  // read character (non-blocking)
+    int c = getchar(); // read character (non-blocking)
 
-  tcsetattr( STDIN_FILENO, TCSANOW, &oldt);  // restore old settings
-  return c;
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt); // restore old settings
+    return c;
 }
 
 constexpr uint8_t UP = 65;
@@ -25,25 +25,40 @@ constexpr uint8_t DOWN = 66;
 constexpr uint8_t LEFT = 68;
 constexpr uint8_t RIGHT = 67;
 
-int main(int argc, char ** argv)
+int main(int argc, char **argv)
 {
     ros::init(argc, argv, "client");
     ros::NodeHandle node;
     ros::Publisher pub = node.advertise<client::Direction>("/roguelike/control", 10);
 
-    while (true) {
+    while (true)
+    {
         int c = getch();
         client::Direction msg;
 
-        switch (c) {
-            case UP:    msg.dx = 0; msg.dy = -1; break;
-            case DOWN:  msg.dx = 0; msg.dy = 1;  break; 
-            case LEFT:  msg.dx = -1; msg.dy = 0; break;
-            case RIGHT: msg.dx = 1; msg.dy = 0;  break;
-            default: continue;
+        switch (c)
+        {
+        case UP:
+            msg.dx = 0;
+            msg.dy = -1;
+            break;
+        case DOWN:
+            msg.dx = 0;
+            msg.dy = 1;
+            break;
+        case LEFT:
+            msg.dx = -1;
+            msg.dy = 0;
+            break;
+        case RIGHT:
+            msg.dx = 1;
+            msg.dy = 0;
+            break;
+        default:
+            continue;
         }
 
-	pub.publish(msg);
+        pub.publish(msg);
     }
 
     return 0;
--- valin_gleb/1/src/server/src/server_node.cpp
+++ valin_gleb/1/src/server/src/server_node.cpp
@@ -10,8 +10,8 @@
 
 class Drawable
 {
-public:
-    virtual void draw(sf::RenderWindow&) = 0;
+  public:
+    virtual void draw(sf::RenderWindow &) = 0;
 };
 
 class Map : public Drawable
@@ -21,20 +21,26 @@ class Map : public Drawable
     std::tuple<int, int> spawn;
     std::vector<std::tuple<int, int>> wall;
     sf::Texture texture;
-public:
-    Map() {
+
+  public:
+    Map()
+    {
         std::ifstream in("res/map.txt");
         in >> width >> height;
 
         std::string row;
         std::getline(in, row);
-        for (int j = 0; j < height; ++j) {
+        for (int j = 0; j < height; ++j)
+        {
             std::getline(in, row);
-            for (int i = 0; i < width; ++i) {
-                if (row[i] == '#') {
+            for (int i = 0; i < width; ++i)
+            {
+                if (row[i] == '#')
+                {
                     wall.push_back(std::make_tuple(i, j));
                 }
-                if (row[i] == '@') {
+                if (row[i] == '@')
+                {
                     spawn = std::make_tuple(i, j);
                 }
             }
@@ -43,8 +49,10 @@ public:
         texture.loadFromFile("res/wall.png");
     }
 
-    void draw(sf::RenderWindow& window) override {
-        for (auto block : wall) {
+    void draw(sf::RenderWindow &window) override
+    {
+        for (auto block : wall)
+        {
             sf::RectangleShape shape(sf::Vector2f(16, 16));
             shape.setPosition(16 * std::get<0>(block), 16 * std::get<1>(block));
             shape.setTexture(&texture);
@@ -54,8 +62,10 @@ public:
 
     bool isFreePos(const std::tuple<int, int> &pos)
     {
-        for(auto b : wall) {
-            if (b == pos) {
+        for (auto b : wall)
+        {
+            if (b == pos)
+            {
                 return false;
             }
         }
@@ -75,21 +85,21 @@ class Hero : public Drawable
     int y;
     sf::Texture texture;
 
-public:
-    Hero(const std::tuple<int, int>& pos)
+  public:
+    Hero(const std::tuple<int, int> &pos)
     {
         x = std::get<0>(pos);
         y = std::get<1>(pos);
         texture.loadFromFile("res/hero.png");
     }
 
-    void place(std::tuple<int, int>& pos)
+    void place(std::tuple<int, int> &pos)
     {
         x = std::get<0>(pos);
         y = std::get<1>(pos);
     }
 
-    void draw(sf::RenderWindow& window) override
+    void draw(sf::RenderWindow &window) override
     {
         sf::RectangleShape shape(sf::Vector2f(16, 16));
         shape.setPosition(16 * x, 16 * y);
@@ -97,7 +107,7 @@ public:
         window.draw(shape);
     }
 
-    std::tuple<int, int> getPos()  { return std::make_tuple(x, y); }
+    std::tuple<int, int> getPos() { return std::make_tuple(x, y); }
 };
 
 class Game : Drawable
@@ -105,46 +115,49 @@ class Game : Drawable
     Map map;
     std::unique_ptr<Hero> hero;
 
-public:
+  public:
     Game() : map()
     {
         hero.reset(new Hero(map.getSpawnPos()));
     }
 
-    void on_control(const client::Direction& msg)
+    void on_control(const client::Direction &msg)
     {
         ROS_INFO("CB!");
-        
+
         std::tuple<int, int> pos = hero->getPos();
         pos = std::make_tuple(std::get<0>(pos) + msg.dx, std::get<1>(pos) + msg.dy);
 
-        if (map.isFreePos(pos)) {
+        if (map.isFreePos(pos))
+        {
             hero->place(pos);
         }
     }
 
-    void draw(sf::RenderWindow& window) override
+    void draw(sf::RenderWindow &window) override
     {
         map.draw(window);
         hero->draw(window);
     }
 };
 
-int main(int argc, char ** argv)
+int main(int argc, char **argv)
 {
     ros::init(argc, argv, "server");
     ros::Time::init();
     ros::Rate rate(60);
     ros::NodeHandle node;
-    
+
     sf::RenderWindow window(sf::VideoMode(800, 600), "roguelike", sf::Style::Titlebar | sf::Style::Close);
     Game game;
 
     ros::Subscriber sub = node.subscribe("/roguelike/control", 10, &Game::on_control, &game);
-    
-    while (window.isOpen()) {
+
+    while (window.isOpen())
+    {
         sf::Event event;
-        while (window.pollEvent(event)) {
+        while (window.pollEvent(event))
+        {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
@@ -157,5 +170,5 @@ int main(int argc, char ** argv)
         rate.sleep();
     }
 
-	return 0;
+    return 0;
 }
