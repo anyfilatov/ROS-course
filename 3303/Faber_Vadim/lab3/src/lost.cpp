fb34ad2e2c5dca272dc2b29903056b1042ffa688
faber-v <32452658+faber-v@users.noreply.github.com>
2018-12-22 03:47:26 -0800

    Update lost.cpp

--- 3303/Faber_Vadim/lab3/src/lost.cpp
+++ 3303/Faber_Vadim/lab3/src/lost.cpp
@@ -3,13 +3,125 @@
 #include <tf/transform_broadcaster.h>
 #include <tf/transform_listener.h>
 
+#include <ros/ros.h>
+#include <tf/transform_listener.h>
+#include <tf/transform_broadcaster.h>
+#include "utilities.h"
+
+
+bool call_for_help() {
+
+	publish_coordinates(lost_robot);
+	return get_robot_coordinates(finder_robot);
+	
+}
+
+void follow(float speed) {
+	
+	get_robot_coordinates(finder_robot);
+	calculate_new_coordinates(lost_robot, finder_robot, speed);
+	ROS_INFO("My coordinates: ( %.2f ; %.2f ; %.2f )", lost_robot.x, lost_robot.y, lost_robot.z);
+}
+
+
+int main(int argc, char **argv) {
+
+	ros::init(argc, argv, "lost");
+    ros::NodeHandle n;
+    ros::Rate r(1);
+    ros::Publisher marker_pub = n.advertise<visualization_msgs::Marker>("lost_topic", 10, true);
+    
+    lost_robot.id = 0;
+    lost_robot.name = "lost";
+    lost_robot.x = 10;
+    lost_robot.y = 10;
+    lost_robot.z = 0;
+    
+	marker_pub.publish(create_marker(lost_robot));
+	
+	bool flag = false;
+	do {
+		flag = call_for_help();
+		
+		r.sleep();
+	} while (!flag);
+	
+	r.sleep();
+	
+	ROS_INFO("He found me! I am following him now");
+	
+	bool stop = false;
+	do {
+		follow(1.0);
+		marker_pub.publish(create_marker(lost_robot));
+		r.sleep();
+	} while (!stop);
+	
+	return 0;
+}
 
 visualization_msgs::Marker setMarker(float x, float y, float z){
 	visualization_msgs::Marker marker;
 	marker.header.frame_id = "/my_map";
 	marker.header.stamp = ros::Time::now();
 	marker.ns = "robots";
-	marker.id = 0;
+	marker.id = 0;#include <ros/ros.h>
+#include <tf/transform_listener.h>
+#include <tf/transform_broadcaster.h>
+#include "utilities.h"
+
+
+bool call_for_help() {
+
+	publish_coordinates(lost_robot);
+	return get_robot_coordinates(finder_robot);
+	
+}
+
+void follow(float speed) {
+	
+	get_robot_coordinates(finder_robot);
+	calculate_new_coordinates(lost_robot, finder_robot, speed);
+	ROS_INFO("My coordinates: ( %.2f ; %.2f ; %.2f )", lost_robot.x, lost_robot.y, lost_robot.z);
+}
+
+
+int main(int argc, char **argv) {
+
+	ros::init(argc, argv, "lost");
+    ros::NodeHandle n;
+    ros::Rate r(1);
+    ros::Publisher marker_pub = n.advertise<visualization_msgs::Marker>("lost_topic", 10, true);
+    
+    lost_robot.id = 0;
+    lost_robot.name = "lost";
+    lost_robot.x = 10;
+    lost_robot.y = 10;
+    lost_robot.z = 0;
+    
+	marker_pub.publish(create_marker(lost_robot));
+	
+	bool flag = false;
+	do {
+		flag = call_for_help();
+		
+		r.sleep();
+	} while (!flag);
+	
+	r.sleep();
+	
+	ROS_INFO("He found me! I am following him now");
+	
+	bool stop = false;
+	do {
+		follow(1.0);
+		marker_pub.publish(create_marker(lost_robot));
+		r.sleep();
+	} while (!stop);
+	
+	return 0;
+}
+
 	marker.type = visualization_msgs::Marker::POINTS;
 	marker.action = visualization_msgs::Marker::ADD;
 	marker.pose.position.x = 0;
