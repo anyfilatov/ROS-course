a7d47c3c8b4b9d2932347bbcbcbf3ba12e6e7277
Aleksandr <lav09.01.95@gmail.com>
2019-01-21 07:48:23 +0300

    lab5

---
+++ 3304/leonchik_aleksandr/lab5/src/tf_broadcaster.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <ros/ros.h>
+#include <tf/transform_broadcaster.h>
+#include <gazebo_msgs/ModelState.h>
+#include <iostream>
+
+using namespace ros;
+std::string NodeName;
+
+void Callback(const gazebo_msgs::ModelState msg)
+{
+	if(NodeName.compare(msg.model_name)!=0)
+		return;
+    	static tf::TransformBroadcaster br;
+    	tf::Transform transform;
+    	transform.setOrigin(tf::Vector3(msg.pose.position.x, msg.pose.position.y, 1.0));
+	br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "world", NodeName));
+}
+
+int main(int argc, char** argv)
+{
+	init(argc, argv, "tf_broadcaster");
+  	if (argc != 2)
+	{
+		ROS_ERROR("Enter robot name");
+		return -1;
+	};
+  	NodeName = argv[1];
+  	NodeHandle node;
+  	Subscriber sub = node.subscribe("gazebo/set_model_state", 10, &Callback);
+	spin();
+  	return 0;
+}
